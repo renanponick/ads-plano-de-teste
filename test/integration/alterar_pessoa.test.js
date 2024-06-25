ba8320386fb9f3c4b7bd451f41277823a3604064
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-06-25 12:43:15 -0300

    new commit

--- test/integration/alterar_pessoa.test.js
+++ test/integration/alterar_pessoa.test.js
@@ -15,25 +15,25 @@ describe('Teste de Alterar pessoa', () => {
         console.info('Encerrando os testes')
     });
 
-    // it('Deve alterar uma pessoa por meio do nome', async () => {
-    //     const mockPessoa = {
-    //       nome: "Alice Da Cunha da Neta de Jesus",
-    //       email: "alicecunhajesus@gmail.com",
-    //       senha: "abacate123"
-    //     };
+    it('Deve alterar uma pessoa por meio do nome', async () => {
+        const mockPessoa = {
+          nome: "Alice Da Cunha da Neta de Jesus",
+          email: "alicecunhajesus@gmail.com",
+          senha: "abacate123"
+        };
       
-    //     const pessoaAdicionada = await servico.Adicionar(mockPessoa, this.transaction);
-    //     const nomeDaPessoa = mockPessoa.nome;
+        const pessoaAdicionada = await servico.Adicionar(mockPessoa, this.transaction);
+        const nomeDaPessoa = mockPessoa.nome;
       
-    //     const mockPessoaUpdate = {
-    //       email: "updated@domain.com",
-    //       senha: "update#password"
-    //     };
+        const mockPessoaUpdate = {
+          email: "updated@domain.com",
+          senha: "update#password"
+        };
       
-    //     const dataValue = await servico.Alterar(nomeDaPessoa, mockPessoaUpdate, this.transaction);
+        const dataValue = await servico.Alterar(nomeDaPessoa, mockPessoaUpdate, this.transaction);
 
-    //     expect(nomeDaPessoa).toBe(dataValue.dataValues.nome); // O nome não deve ter mudado
-    //     expect(mockPessoaUpdate.email).toBe(dataValue.dataValues.email);
-    //     expect(mockPessoaUpdate.senha).toBe(dataValue.dataValues.senha);
-    // });       
+        expect(nomeDaPessoa).toBe(dataValue.dataValues.nome); // O nome não deve ter mudado
+        expect(mockPessoaUpdate.email).toBe(dataValue.dataValues.email);
+        expect(mockPessoaUpdate.senha).toBe(dataValue.dataValues.senha);
+    });       
 })
--- test/integration/deletar.test.js
+++ test/integration/deletar.test.js
@@ -17,21 +17,21 @@ describe('Teste de Deletar pessoa', () => {
         console.info('Encerrando os testes');
     });
 
-    // it('Deve deletar uma pessoa por meio do id', async () => {
-    //     const mockPessoa = {
-    //         nome: "Alice Da Cunha da Neta de Jesus",
-    //         email: "alicecunhajesus@gmail.com",
-    //         senha: "abacate123"
-    //     };
+    it('Deve deletar uma pessoa por meio do id', async () => {
+        const mockPessoa = {
+            nome: "Alice Da Cunha da Neta de Jesus",
+            email: "alicecunhajesus@gmail.com",
+            senha: "abacate123"
+        };
 
-    //     // Adicionar a pessoa inicialmente
-    //     const pessoaAdicionada = await servico.Adicionar(mockPessoa, transaction);
-    //     const id = pessoaAdicionada.id;
+        // Adicionar a pessoa inicialmente
+        const pessoaAdicionada = await servico.Adicionar(mockPessoa, transaction);
+        const id = pessoaAdicionada.id;
 
-    //     // Deletar a pessoa pelo ID
-    //     const dataValue = await servico.Deletar(id, transaction);
+        // Deletar a pessoa pelo ID
+        const dataValue = await servico.Deletar(id, transaction);
 
-    //     // Verificações
-    //     expect(dataValue).toBe(1); // Espera-se que uma pessoa tenha sido deletada
-    // });
+        // Verificações
+        expect(dataValue).toBe(1); // Espera-se que uma pessoa tenha sido deletada
+    });
 });
